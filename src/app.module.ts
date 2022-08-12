c7550050e4d1785b3f23b542a8fed80be8ec098f
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 15:51:05 -0300

    remove wrong controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { PixModule } from './pix/pix.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TransactionModule } from './transaction/transaction.module';
-import { ServiceController } from './service/service.controller';
 
 @Module({
-  imports: [PixModule, PrismaModule, TransactionModule],
-  controllers: [ServiceController],
+  imports: [PixModule, PrismaModule, TransactionModule]  
 })
 export class AppModule {}
