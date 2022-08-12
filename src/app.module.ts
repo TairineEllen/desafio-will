84a30dbe3b114f966389d0822ad64520a19cb9cf
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 15:38:24 -0300

    create transaction module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PixModule } from './pix/pix.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { ServiceController } from './service/service.controller';
 
 @Module({
-  imports: [PixModule, PrismaModule],
+  imports: [PixModule, PrismaModule, TransactionModule],
+  controllers: [ServiceController],
 })
 export class AppModule {}
---
+++ src/transaction/transaction.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TransactionController } from './transaction.controller';
+import { TransactionService } from './transaction.service';
+
+@Module({
+  controllers: [TransactionController],
+  providers: [TransactionService]
+})
+export class TransactionModule {}
