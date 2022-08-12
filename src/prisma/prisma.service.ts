73512cd69e99ceddc350ae5f0591adcf475031ab
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 19:50:24 -0300

    create method to clean the database

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -12,4 +12,11 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+
+  cleanDb() {
+    return this.$transaction([
+      this.pix.deleteMany(),
+      this.transaction.deleteMany(),
+    ]);
+  }
 }
