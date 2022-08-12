691a0278e523ba347aa5d555e19214200c0f944a
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 11:15:53 -0300

    create prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PixModule } from './pix/pix.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PixModule]
+  imports: [PixModule, PrismaModule]
 })
 export class AppModule {}
