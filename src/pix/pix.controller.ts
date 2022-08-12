36c54f7410e10ae2fd45aa5a37e32a048daa32fc
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 11:47:46 -0300

    create pix dto

--- src/pix/pix.controller.ts
+++ src/pix/pix.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Body } from '@nestjs/common';
 import { PixService } from './pix.service';
+import { Request } from 'express';
+import { PixDto } from './dto';
 
 @Controller('pix')
 export class PixController {
   constructor (private pixService: PixService) {}
 
   @Post('create')
-  createPix() {
+  createPix(@Body() dto: PixDto) {
+    console.log('AQUI o', dto)
     return 'Pix created!';
   }
 
