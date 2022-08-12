a7e5e878f095d3345ae632d93f0625a608361c68
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 20:02:44 -0300

    create swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('API de PIX')
+  .setDescription('API rest para o time de backoffice gerenciar transações de Pix')
+  .setVersion('1.0')
+  .addTag('pix')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
