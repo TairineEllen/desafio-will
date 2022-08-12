9f8c1bb2758abe0db1a6d994c9d934ce89057d7e
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 12:23:38 -0300

    create method to create a pix

--- src/pix/pix.service.ts
+++ src/pix/pix.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PixDto } from './dto';
 
 @Injectable()
 export class PixService {
-  createPix(dto) {
-    return 'oi'
+  constructor(private prisma: PrismaService) {}
+
+  async createPix(dto: PixDto) {
+    const pix = await this.prisma.pix.create({
+      data: {
+        key: dto.key
+      }
+    });
+    return pix;
   }
 
   getPixes() {}
