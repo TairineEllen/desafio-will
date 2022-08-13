fc2b45afafe550ed5f2f86c268afb16eb147d03c
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 21:20:24 -0300

    create test for create a pix and for list all pixes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,62 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing'
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import { PixDto } from 'src/pix/dto';
+import { assert } from 'console';
+import { doesNotMatch } from 'assert';
+import exp from 'constants';
 
-describe('AppController (e2e)', () => {
+
+describe('Test end 2 end', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();    
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
   });
-});
+
+  describe('Pix tests', () => {
+    describe('Create a pix', () => {
+      const dto: PixDto = {
+        key: 'pix@will.com'
+      }
+      it('should create a pix', (done) => {
+        request(app.getHttpServer()).post('/pix/create').send(dto).expect(201).end((error, res) => {
+          if (error) return done(error);
+          return done();
+        });
+    });
+
+    });
+
+    describe('Get all pixes', () => {
+      it('should list all pixes', async () => {
+        const response = await request(app.getHttpServer()).get('/pix/');
+        expect(response.status).toEqual(200);
+        expect(response.body[0].key).toEqual('pix@will.com');
+      });
+    });
+   
+    
+  });
+
+  describe('Transactions tests', () => {});
+})
