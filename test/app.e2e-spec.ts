7b8a794541e3d1fc36f056744ff1d95a9b575974
TairineEllen <tairine.ellen@gmail.com>
2022-08-12 22:40:53 -0300

    add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,10 +3,8 @@ import { Test } from '@nestjs/testing'
 import * as request from 'supertest';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
-import { PixDto } from 'src/pix/dto';
-import { assert } from 'console';
-import { doesNotMatch } from 'assert';
-import exp from 'constants';
+import { EditPixDto, PixDto } from 'src/pix/dto';
+import { TransactionDto } from 'src/transaction/dto';
 
 
 describe('Test end 2 end', () => {
@@ -26,7 +24,7 @@ describe('Test end 2 end', () => {
 
     prisma = app.get(PrismaService);
 
-    await prisma.cleanDb();    
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
@@ -39,24 +37,69 @@ describe('Test end 2 end', () => {
         key: 'pix@will.com'
       }
       it('should create a pix', (done) => {
-        request(app.getHttpServer()).post('/pix/create').send(dto).expect(201).end((error, res) => {
-          if (error) return done(error);
-          return done();
-        });
-    });
-
+        request(
+          app.getHttpServer())
+          .post('/pix/create')
+          .send(dto)
+          .expect(201)
+          .end((error, res) => {
+            if (error) return done(error);
+            return done();
+          });
+      });
     });
 
     describe('Get all pixes', () => {
       it('should list all pixes', async () => {
-        const response = await request(app.getHttpServer()).get('/pix/');
+        const response = await request(app.getHttpServer()).get('/pix');
         expect(response.status).toEqual(200);
         expect(response.body[0].key).toEqual('pix@will.com');
       });
     });
-   
-    
+
+    describe('Edit a pix', () => {
+      const newDto: EditPixDto = {
+        key: 'lili@dog.com'
+      };
+
+      it('should edit a pix', async () => {
+        const response = await request(app.getHttpServer()).get('/pix/');
+
+        await request(app.getHttpServer())
+        .put(`/pix/${response.body[0].id}`)
+        .send(newDto)
+        .expect(200)
+      });
+    });
+
+    describe('Delete a pix', () => {
+      it('should delete a pix', async () => {
+        const response = await request(app.getHttpServer()).get('/pix/');
+
+        await request(app.getHttpServer())
+        .delete(`/pix/${response.body[0].id}`)
+        .expect(200)
+      })
+    })
+
   });
 
-  describe('Transactions tests', () => {});
+  describe('Transactions tests', () => {
+    it('should be able to create a transaction', async () => {
+      const dto: PixDto = {
+        key: 'pix@will.com'
+      }
+
+      await request(app.getHttpServer())
+        .post('/pix/create')
+        .send(dto)
+
+      const transactionDto: TransactionDto = {
+        key: 'pix@will.com',
+        amount: 25
+      }
+
+      await request(app.getHttpServer()).post('/transactions/new').send(transactionDto).expect(201)
+    })
+   });
 })
